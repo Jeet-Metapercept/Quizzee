64799affd0f44f55d9de796e537bcf738e000d10
Jeet-Metapercept <jeetp@proximabiz.com>
2023-11-26 17:25:30 +0530

    🎉 Quizzee

---
+++ nuxt.config.ts
@@ -0,0 +1,47 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    // https://nuxt.com/docs/guide/going-further/runtime-config#exposing
+    public: {
+      PROJECT_NAME: process.env.PROJECT_NAME,
+      BASEURL: process.env.BASE_URL || 'http://localhost:3000',
+    },
+  },
+  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@nuxt/image', 'nuxt-icon', '@nuxtjs/device', '@morev/vue-transitions/nuxt', ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }], '@pinia-plugin-persistedstate/nuxt'],
+  hooks: {
+    'components:dirs': (dirs) => {
+      dirs.unshift({
+        path: '~/components/ui',
+        extensions: ['.vue'],
+        prefix: 'Ui',
+        pathPrefix: false,
+      })
+    },
+  },
+  app: {
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      charset: 'utf-16',
+      title: process.env.PROJECT_NAME,
+      meta: [
+        {
+          name: 'description',
+          content: 'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
+        },
+      ],
+      viewport:
+        'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono&display=swap',
+        },
+      ],
+    },
+  },
+})
