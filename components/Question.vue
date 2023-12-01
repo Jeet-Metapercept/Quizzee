b961317a7c480f430bb55ccd11dbe73e7f513e43
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-02 03:13:49 +0530

    question component

---
+++ components/Question.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Toggle } from '@/components/ui/toggle'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+
+// interface Props {
+//   open: boolean
+//   loading: boolean
+// }
+// const props = defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <label tabindex="0" class="border-border-highlight bg-accent-selected-bg z-10 text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border focus:outline-none">
+      <Toggle size="lg" aria-label="Toggle italic" class="justify-start h-14 px-2">
+        <span class="flex items-center gap-2">
+          <Avatar>
+            <AvatarFallback class="text-lg text-muted-foreground font-semibold">A</AvatarFallback>
+          </Avatar>
+          <span>299 792 458 m / s</span>
+        </span>
+      </Toggle>
+    </label>
+  </div>
+</template>
--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -1,24 +1,21 @@
 <script setup lang="ts">
-import { usePageviewStore } from '@/stores/pageview'
 import { Toggle } from '@/components/ui/toggle'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import Question from '@/components/Question.vue'
 
 definePageMeta({
   layout: 'default',
 })
-
-const VIEWS = usePageviewStore()
-VIEWS.incrementView()
-VIEWS.updateLastSeen()
 </script>
 
 <template>
   <div class="h-screen flex items-center justify-center">
     <div class="mx-auto max-w-7xl w-full p-0 md:max-w-lg">
       <div class="w-full">
-        <div class="formbricks-form w-full">
+        <div class="quizze-form w-full">
           <div class="w-full">
             <div class="flex flex-col justify-between bg-[--fb-survey-background-color] px-6 pb-3 pt-6">
-              <div class="my-auto">
+              <div class="my-auto  border-1 rounded border-dark-500 shadow-md p-4">
                 <form class="w-full">
                   <label class="text-heading mb-1.5 block text-base font-semibold leading-6">
                     <div class="flex items-center mr-[3ch] justify-between">What is Speed of Light</div>
@@ -29,34 +26,16 @@ VIEWS.updateLastSeen()
                       <legend class="sr-only">
                         Options
                       </legend>
-                      <div role="radiogroup" class="bg-survey-bg relative max-h-[42vh] space-y-2 overflow-y-auto rounded-md py-0.5 pr-2">
-                        <Toggle size="lg" variant="outline" aria-label="Toggle italic">
-                          299 792 458 m / s
-                        </Toggle>
-                        <label tabindex="1" class="border-border-highlight bg-accent-selected-bg z-10 text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border p-4 focus:outline-none">
-                          <span class="flex items-center text-sm">
-                            <input type="radio" name="speed_of_light" value="299 792 458 m / s">
-                            <span class="ml-3 font-medium">299 792 458 m / s</span>
-                          </span>
-                        </label>
-                        <label tabindex="2" class="border-border text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border p-4 focus:outline-none">
-                          <span class="flex items-center text-sm">
-                            <input type="radio" name="speed_of_light" value="399 792 458 m / s">
-                            <span class="ml-3 font-medium">399 792 458 m / s</span>
-                          </span>
-                        </label>
-                        <label tabindex="3" class="border-border text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border p-4 focus:outline-none">
-                          <span class="flex items-center text-sm">
-                            <input type="radio" name="speed_of_light" value="299 297 458 m / s">
-                            <span class="ml-3 font-medium">299 297 458 m / s</span>
-                          </span>
-                        </label>
-                        <label tabindex="4" class="border-border text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border p-4 focus:outline-none">
+                      <div class="bg-survey-bg relative max-h-[42vh] space-y-2 overflow-y-auto rounded-md py-0.5 pr-2">
+                        <!-- answer #1 -->
+                        <Question />
+
+                        <!-- <label tabindex="4" class="border-border text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border p-4 focus:outline-none">
                           <span class="flex items-center text-sm">
                             <input type="radio" name="speed_of_light" value="299 792 854 m / s">
                             <span class="ml-3 font-medium">299 792 854 m / s</span>
                           </span>
-                        </label>
+                        </label> -->
                       </div>
                     </fieldset>
                   </div>
