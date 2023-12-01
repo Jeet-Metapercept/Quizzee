cb4406ed93b33600cac4602a19d43bf48722e032
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-02 03:20:37 +0530

    quiz component stucture

---
+++ pages/quiz/components/Card.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import QOption from './Option.vue'
+</script>
+
+<template>
+  <div class="quizze-form w-full">
+    <div class="w-full">
+      <div class="flex flex-col justify-between bg-[--fb-survey-background-color] px-6 pb-3 pt-6">
+        <div class="my-auto  border-1 rounded border-dark-500 shadow-md p-4">
+          <form class="w-full">
+            <label class="text-heading mb-1.5 block text-base font-semibold leading-6">
+              <div class="flex items-center mr-[3ch] justify-between">What is Speed of Light</div>
+            </label>
+            <label class="text-subheading block text-sm font-normal leading-6">Can't do both.</label>
+            <div class="mt-4">
+              <fieldset>
+                <legend class="sr-only">
+                  Options
+                </legend>
+                <div class="bg-survey-bg relative max-h-[42vh] space-y-2 overflow-y-auto rounded-md py-0.5 pr-2">
+                  <QOption />
+
+                  <!-- <label tabindex="4" class="border-border text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border p-4 focus:outline-none">
+                          <span class="flex items-center text-sm">
+                            <input type="radio" name="speed_of_light" value="299 792 854 m / s">
+                            <span class="ml-3 font-medium">299 792 854 m / s</span>
+                          </span>
+                        </label> -->
+                </div>
+              </fieldset>
+            </div>
+            <div class="mt-4 flex w-full justify-between">
+              <button type="button" class="border-back-button-border text-heading focus:ring-focus flex items-center rounded-md border px-3 py-3 text-base font-medium leading-4 shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2">
+                Back
+              </button>
+              <div />
+              <button type="submit" class="bg-brand border-submit-button-border text-on-brand focus:ring-focus flex items-center rounded-md border px-3 py-3 text-base font-medium leading-4 shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2">
+                Finish
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/quiz/index.vue
+++ pages/quiz/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { Toggle } from '@/components/ui/toggle'
-import { Avatar, AvatarFallback } from '@/components/ui/avatar'
-import Question from '@/components/Question.vue'
+import QuizCard from './components/Card.vue'
 
 definePageMeta({
   layout: 'default',
@@ -12,47 +10,7 @@ definePageMeta({
   <div class="h-screen flex items-center justify-center">
     <div class="mx-auto max-w-7xl w-full p-0 md:max-w-lg">
       <div class="w-full">
-        <div class="quizze-form w-full">
-          <div class="w-full">
-            <div class="flex flex-col justify-between bg-[--fb-survey-background-color] px-6 pb-3 pt-6">
-              <div class="my-auto  border-1 rounded border-dark-500 shadow-md p-4">
-                <form class="w-full">
-                  <label class="text-heading mb-1.5 block text-base font-semibold leading-6">
-                    <div class="flex items-center mr-[3ch] justify-between">What is Speed of Light</div>
-                  </label>
-                  <label class="text-subheading block text-sm font-normal leading-6">Can't do both.</label>
-                  <div class="mt-4">
-                    <fieldset>
-                      <legend class="sr-only">
-                        Options
-                      </legend>
-                      <div class="bg-survey-bg relative max-h-[42vh] space-y-2 overflow-y-auto rounded-md py-0.5 pr-2">
-                        <!-- answer #1 -->
-                        <Question />
-
-                        <!-- <label tabindex="4" class="border-border text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border p-4 focus:outline-none">
-                          <span class="flex items-center text-sm">
-                            <input type="radio" name="speed_of_light" value="299 792 854 m / s">
-                            <span class="ml-3 font-medium">299 792 854 m / s</span>
-                          </span>
-                        </label> -->
-                      </div>
-                    </fieldset>
-                  </div>
-                  <div class="mt-4 flex w-full justify-between">
-                    <button type="button" class="border-back-button-border text-heading focus:ring-focus flex items-center rounded-md border px-3 py-3 text-base font-medium leading-4 shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2">
-                      Back
-                    </button>
-                    <div />
-                    <button type="submit" class="bg-brand border-submit-button-border text-on-brand focus:ring-focus flex items-center rounded-md border px-3 py-3 text-base font-medium leading-4 shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2">
-                      Finish
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
+        <QuizCard />
       </div>
     </div>
   </div>
