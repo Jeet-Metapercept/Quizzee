63a03bd15b8c672b0d7fdb8488269b138a123472
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-02 04:45:41 +0530

    Update Option.vue

--- pages/quiz/components/Option.vue
+++ pages/quiz/components/Option.vue
@@ -21,17 +21,15 @@ function selectOption() {
 
 <template>
   <div>
-    <label tabindex="0" class="border-border-highlight bg-accent-selected-bg z-10 text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border focus:outline-none">
+    <label tabindex="0" class="border-border-highlight z-10 text-heading focus-within:border-border-highlight focus-within:bg-accent-bg hover:bg-accent-bg relative flex cursor-pointer flex-col rounded-md border focus:outline-none">
       <Toggle size="lg" aria-label="Toggle italic" class="justify-start h-auto px-2 py-2" @update:pressed="selectOption()">
         <span class="flex items-center gap-2">
           <Avatar>
-            <!-- <AvatarFallback class="text-lg text-muted-foreground font-semibold">{{ props.position }}</AvatarFallback> -->
             <AvatarFallback>
               <Icon :name="props.icon" class="text-heading  h-4 w-4" />
             </AvatarFallback>
           </Avatar>
-          <span class="select-none" :class="{ 'text-red-500': isPressed }">{{ option.option }} -- {{ isPressed }}</span>
-          <!-- <span class="select-none" :class="{ 'text-muted-foreground': active }">299 792 458 m {{ active }}/ s</span> -->
+          <span class="select-none" :class="{ 'text-muted-foreground': !isPressed }">{{ option.option }}</span>
         </span>
       </Toggle>
     </label>
