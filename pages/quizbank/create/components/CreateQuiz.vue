e651686049bc3701a222a8f432321ec2e6923d15
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-03 03:46:31 +0530

    Update CreateQuiz.vue

--- pages/quizbank/create/components/CreateQuiz.vue
+++ pages/quizbank/create/components/CreateQuiz.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
 import { Textarea } from '@/components/ui/textarea'
@@ -6,17 +7,38 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { useQuestionStore } from '@/stores/questionbank'
 
+const STORE = useQuestionStore()
 const faker = useFaker()
 const isComplete = ref(false)
 const isLoading = ref(false)
 
+// Categories
+const categories = computed(() => STORE.GET_CATEGORIES)
+const isSategoriesOpen = ref(false)
+const selectedCategory = ref(categories.value[0])
+function filterCategoryFunction(val: string[], search: string) {
+  return val.filter(item => item.toLowerCase().includes(search.toLowerCase()))
+}
 const quiz = ref({
   title: faker.generateQuizName(),
   description: '',
@@ -50,13 +72,13 @@ const quiz = ref({
         <div class="flex gap-4 items-start">
           <div class="w-[110px] h-[110px] flex items-center justify-center rounded border cursor-pointer">
             <!-- <Icon name="material-symbols:add-photo-alternate-outline" class="w-12 h-12 text-slate-200" /> -->
-            <NuxtImg
+            <img
               alt="quiz-logo"
               :src="`https://api.dicebear.com/7.x/shapes/svg?seed=${quiz.title}`"
               class="block rounded"
               :height="110"
               :width="110"
-            />
+            >
           </div>
 
           <div class="flex flex-col flex-grow">
@@ -68,6 +90,51 @@ const quiz = ref({
               :disabled="isLoading"
             />
           </div>
+        </div>
+
+        <div class="grid gap-2">
+          <Label for="category">Category</Label>
+          <Popover v-model:open="isSategoriesOpen">
+            <PopoverTrigger as-child>
+              <Button
+                variant="outline"
+                role="combobox"
+                :aria-expanded="isSategoriesOpen"
+                class=" justify-between"
+              >
+                {{ selectedCategory ? selectedCategory : 'Select Category' }}
+                <Icon name="radix-icons:chevron-down" class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent class="p-0">
+              <Command :filter-function="filterCategoryFunction">
+                <CommandInput placeholder="Search category..." />
+                <CommandEmpty>No category found.</CommandEmpty>
+                <CommandList>
+                  <CommandGroup>
+                    <CommandItem
+                      v-for="c in categories"
+                      :key="c"
+                      :value="c"
+                      @select="(ev) => {
+                        selectedCategory = ev.detail.value!
+                        isSategoriesOpen = false
+                      }"
+                    >
+                      <Icon
+                        name="radix-icons:check" :class="cn(
+                          'mr-2 h-4 w-4',
+                          selectedCategory === c ? 'opacity-100' : 'opacity-0',
+                        )"
+                      />
+
+                      {{ c }}
+                    </CommandItem>
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
         </div>
       </CardContent>
     </Card>
