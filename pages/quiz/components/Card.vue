01530cef64b1ae4527bcba38b2ba9ecb03364106
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-02 22:02:24 +0530

    Update Card.vue

--- pages/quiz/components/Card.vue
+++ pages/quiz/components/Card.vue
@@ -43,7 +43,7 @@ function handleOptionSelected(selectedOption: OptionType & { index: number; is_s
               </div>
             </label> -->
 
-            <label class="text-xs block text-muted-foreground font-semibold border-b border-dashed mt-4 mb-4">
+            <label class="block font-mono text-xs text-slate-400 border-b border-dashed mt-4 mb-4">
               <div class="flex items-center justify-between py-1.5">
                 <span>Question</span>
                 <span> 1/10</span>
