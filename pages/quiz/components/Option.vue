24c1286dce8d4337cd225ba3aaafd401f22647fc
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-02 13:38:31 +0530

    Update Option.vue

--- pages/quiz/components/Option.vue
+++ pages/quiz/components/Option.vue
@@ -21,7 +21,10 @@ function selectOption() {
 
 <template>
   <div>
-    <label tabindex="0" class="border-border-highlight z-10 text-heading focus-within:border-border-highlight focus-within:bg-accent-bg flex cursor-pointer flex-col rounded-md border focus:outline-none">
+    <label
+      tabindex="0" class="border-border-highlight z-10 text-heading focus-within:border-border-highlight focus-within:bg-accent-bg flex cursor-pointer flex-col rounded-md border focus:outline-none hover:border-slate-600"
+      :class="{ 'border-slate-600': isPressed }"
+    >
       <Toggle size="lg" :aria-label="props.option.option" class="justify-start h-auto px-4 py-2" @update:pressed="selectOption()">
         <span class="flex items-center gap-4">
           <Avatar>
