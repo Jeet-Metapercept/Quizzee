f77dcd226f465692485ed5b90fe485ca47cc8284
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-08 00:04:24 +0530

    Update Prepare.vue

--- pages/quiz/components/Prepare.vue
+++ pages/quiz/components/Prepare.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
@@ -11,9 +11,16 @@
         </div>
         <span class="bg-slate-300 mb-[10px] inline-block h-1 w-16 rounded-[100%]" />
         <div>
-          <label for="thankYouCard" class="text-heading mb-1.5 block text-base font-semibold leading-6">
+          <label for="prepare" class="text-heading mb-1.5 block text-base font-semibold leading-6">
             <div class="flex items-center  justify-center">Quiz Title</div>
-          </label><label for="thankYouCard" class="text-subheading block text-sm font-normal leading-6">Preparing quiz, please wait...</label>
+          </label><label for="prepare" class="text-subheading block text-sm font-normal leading-6">Preparing quiz, please wait...</label>
+        </div>
+
+        <div class="m-5">
+          <Button type="submit" variant="default" class="w-full">
+            Start
+            <Icon name="lucide:move-right" class="ms-2" />
+          </Button>
         </div>
       </div>
     </div>
