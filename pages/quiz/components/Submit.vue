1573b0801fda12ad8f5608e71f03a0ccdb77e6d6
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-10 01:29:18 +0530

    Update Submit.vue

--- pages/quiz/components/Submit.vue
+++ pages/quiz/components/Submit.vue
@@ -17,20 +17,24 @@ const status = computed(() => QUIZ_STORE.GET_QUIZ_STATUS)
             <Icon v-else name="radix-icons:info-circled" class="w-24 h-24 text-red-600 rounded-full" />
           </div>
           <span class="bg-slate-300 mb-[10px] inline-block h-1 w-16 rounded-[100%]" />
-          <div v-if="status === 'complete'">
-            <label for="result" class="text-heading mb-1.5 block text-base font-semibold leading-6">
-              <div class="flex items-center justify-center">That's a wrap!</div>
-            </label><label for="result" class="text-muted-foreground block text-sm font-normal leading-6">We appreciate your participation.</label>
-          </div>
-          <div v-else>
-            <label for="waiting-for-result" class="text-heading mb-1.5 block text-base font-semibold leading-6">
-              <div class="flex items-center justify-center">Analyzing Your Answers</div>
-            </label><label for="waiting-for-result" class="text-muted-foreground block text-sm font-normal leading-6">Hold tight - we're preparing your score card.</label>
-          </div>
+          <transition-fade v-if="status === 'complete'" appear>
+            <div>
+              <label for="result" class="text-heading mb-1.5 block text-base font-semibold leading-6">
+                <div class="flex items-center justify-center">That's a wrap!</div>
+              </label><label for="result" class="text-muted-foreground block text-sm font-normal leading-6">We appreciate your participation.</label>
+            </div>
+          </transition-fade>
+          <transition-fade v-else appear>
+            <div>
+              <label for="waiting-for-result" class="text-heading mb-1.5 block text-base font-semibold leading-6">
+                <div class="flex items-center justify-center">Analyzing Your Answers</div>
+              </label><label for="waiting-for-result" class="text-muted-foreground block text-sm font-normal leading-6">Hold tight - we're preparing your score card.</label>
+            </div>
+          </transition-fade>
         </div>
 
         <div class="min-h-[40px]">
-          <transition-fade :delay="1000" appear>
+          <transition-fade appear>
             <div v-if="status === 'complete'" class="flex items-center justify-center gap-2 mt-4 ">
               <Button variant="outline" class="rounded-full">
                 Score Card
