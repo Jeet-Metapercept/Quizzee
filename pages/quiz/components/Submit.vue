2d10e750e2fd6b8da3ed65dfb42db8c7cb321ad9
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-10 01:03:48 +0530

    Update Submit.vue

--- pages/quiz/components/Submit.vue
+++ pages/quiz/components/Submit.vue
@@ -9,18 +9,23 @@ const status = computed(() => QUIZ_STORE.GET_QUIZ_STATUS)
   <div class="complete-form">
     <div class="flex items-center justify-center h-full">
       <div class="text-center">
-        <div class="text-brand flex items-center justify-center">
-          <!-- <transition-fade> -->
-          <Icon v-if="status === 'submit'" name="line-md:loading-twotone-loop" class="w-24 h-24 text-green-600 rounded-full" />
-          <Icon v-else-if="status === 'complete'" name="radix-icons:check-circled" class="w-24 h-24 text-green-600 rounded-full" />
-          <Icon v-else name="radix-icons:info-circled" class="w-24 h-24 text-red-600 rounded-full" />
-          <!-- </transition-fade> -->
+        <div class="text-brand flex items-center justify-center min-h-[100px]">
+          <transition-fade>
+            <Icon v-if="status === 'submit'" name="line-md:loading-twotone-loop" class="w-24 h-24 text-muted-foreground rounded-full" />
+            <Icon v-else-if="status === 'complete'" name="radix-icons:check-circled" class="w-24 h-24 text-green-600 rounded-full" />
+            <Icon v-else name="radix-icons:info-circled" class="w-24 h-24 text-red-600 rounded-full" />
+          </transition-fade>
         </div>
         <span class="bg-slate-300 mb-[10px] inline-block h-1 w-16 rounded-[100%]" />
-        <div>
-          <label for="thankYouCard" class="text-heading mb-1.5 block text-base font-semibold leading-6">
-            <div class="flex items-center  justify-center">That's a wrap!</div>
-          </label><label for="thankYouCard" class="text-muted-foreground block text-sm font-normal leading-6">We appreciate your participation.</label>
+        <div v-if="status === 'complete'">
+          <label for="result" class="text-heading mb-1.5 block text-base font-semibold leading-6">
+            <div class="flex items-center justify-center">That's a wrap!</div>
+          </label><label for="result" class="text-muted-foreground block text-sm font-normal leading-6">We appreciate your participation.</label>
+        </div>
+        <div v-else>
+          <label for="waiting-for-result" class="text-heading mb-1.5 block text-base font-semibold leading-6">
+            <div class="flex items-center justify-center">Analyzing Your Answers</div>
+          </label><label for="waiting-for-result" class="text-muted-foreground block text-sm font-normal leading-6">Hold tight - we're preparing your score card.. Almost there!</label>
         </div>
       </div>
     </div>
