8a3d283c37532d80ad0bb7833b3ef8452866ba8e
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-10 01:13:14 +0530

    Update Submit.vue

--- pages/quiz/components/Submit.vue
+++ pages/quiz/components/Submit.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useQuizStore } from '~/stores/quiz'
+import { Button } from '@/components/ui/button'
 
 const QUIZ_STORE = useQuizStore()
 const status = computed(() => QUIZ_STORE.GET_QUIZ_STATUS)
@@ -10,11 +11,11 @@ const status = computed(() => QUIZ_STORE.GET_QUIZ_STATUS)
     <div class="flex items-center justify-center h-full">
       <div class="text-center">
         <div class="text-brand flex items-center justify-center min-h-[100px]">
-          <transition-fade>
-            <Icon v-if="status === 'submit'" name="line-md:loading-twotone-loop" class="w-24 h-24 text-muted-foreground rounded-full" />
-            <Icon v-else-if="status === 'complete'" name="radix-icons:check-circled" class="w-24 h-24 text-green-600 rounded-full" />
-            <Icon v-else name="radix-icons:info-circled" class="w-24 h-24 text-red-600 rounded-full" />
-          </transition-fade>
+          <!-- <transition-fade> -->
+          <Icon v-if="status === 'submit'" name="line-md:loading-twotone-loop" class="w-24 h-24 text-muted-foreground rounded-full" />
+          <Icon v-else-if="status === 'complete'" name="radix-icons:check-circled" class="w-24 h-24 text-green-600 rounded-full" />
+          <Icon v-else name="radix-icons:info-circled" class="w-24 h-24 text-red-600 rounded-full" />
+          <!-- </transition-fade> -->
         </div>
         <span class="bg-slate-300 mb-[10px] inline-block h-1 w-16 rounded-[100%]" />
         <div v-if="status === 'complete'">
@@ -25,7 +26,17 @@ const status = computed(() => QUIZ_STORE.GET_QUIZ_STATUS)
         <div v-else>
           <label for="waiting-for-result" class="text-heading mb-1.5 block text-base font-semibold leading-6">
             <div class="flex items-center justify-center">Analyzing Your Answers</div>
-          </label><label for="waiting-for-result" class="text-muted-foreground block text-sm font-normal leading-6">Hold tight - we're preparing your score card.. Almost there!</label>
+          </label><label for="waiting-for-result" class="text-muted-foreground block text-sm font-normal leading-6">Hold tight - we're preparing your score card.</label>
+        </div>
+
+        <div v-if="status === 'complete'" class="flex items-center justify-center gap-2 mt-4">
+          <Button variant="outline" class="rounded-full">
+            Score Card
+          </Button>
+
+          <Button variant="outline" size="icon" class="rounded-full">
+            <Icon name="radix-icons:reload" />
+          </Button>
         </div>
       </div>
     </div>
