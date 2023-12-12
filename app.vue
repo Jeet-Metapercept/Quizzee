8acce456dfd11abf42457251740ea6a944a22be3
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-13 02:21:10 +0530

    seo changes

--- app.vue
+++ app.vue
@@ -2,8 +2,17 @@
 import Toaster from '@/components/ui/toast/Toaster.vue'
 import { useQuestionStore } from '@/stores/questionbank'
 
-const STORE = useQuestionStore()
+const project = useRuntimeConfig().public.PROJECT_NAME
 
+useSeoMeta({
+  title: project,
+  ogTitle: project,
+  description: `Powered by ${project}`,
+  ogDescription: `Powered by ${project}`,
+  ogImage: './public/android-chrome-512x512.png',
+})
+
+const STORE = useQuestionStore()
 onMounted(() => STORE.FETCH_CATEGORIES())
 </script>
 
