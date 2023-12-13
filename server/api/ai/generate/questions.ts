376bd2875b66980d09f7db962d6533dd0c9139af
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-14 02:45:31 +0530

    prompt engineering

--- server/api/ai/generate/questions.ts
+++ server/api/ai/generate/questions.ts
@@ -1,14 +1,14 @@
 import OpenAI from 'openai'
 import type { User } from '@supabase/supabase-js'
 import type { GenerateQuestionRequest } from './config'
-import { GenerateQuestionSchema, systemPrompt } from './config'
+import { GenerateQuestionSchema, checkPerms, returnUnauthorized, systemPrompt } from './config'
 import { serverSupabaseUser } from '#supabase/server'
 
 const AUTH_REQUIRED = false
 const runtimeConfig = useRuntimeConfig()
 const openai = new OpenAI({
   apiKey: runtimeConfig.OPENAI_API_KEY,
-  timeout: 30 * 1000, // 30 seconds (default is 10 minutes)
+  timeout: 60 * 1000, // 60 seconds (default is 10 minutes)
 })
 
 export default defineEventHandler(async (event) => {
@@ -24,11 +24,11 @@ export default defineEventHandler(async (event) => {
   if (!validationResult.success) {
     throw createError({
       statusCode: 400,
-      statusMessage: 'Invalid request body',
+      statusMessage: 'Invalid request',
       data: validationResult.error.issues,
     })
   }
-
+  console.table(body)
   const system_prompt = systemPrompt({ category: body.category, difficulty: body.difficulty, count: body.count })
   const messages: OpenAI.Chat.ChatCompletionUserMessageParam[] = [{
     role: 'user',
@@ -36,55 +36,33 @@ export default defineEventHandler(async (event) => {
   }]
 
   const params: OpenAI.Chat.ChatCompletionCreateParams = {
-    model: 'gpt-3.5-turbo-1106',
+    model: 'gpt-3.5-turbo',
     messages: [system_prompt, ...messages],
     max_tokens: 2048,
     temperature: 0.5,
     top_p: 1,
     stream: false,
-    response_format: { type: 'json_object' }, // gpt-4-1106-preview or gpt-3.5-turbo-1106
+    response_format: { type: 'text' }, // gpt-4-1106-preview or gpt-3.5-turbo-1106
   }
 
   const chatCompletion: OpenAI.Chat.ChatCompletion = await openai.chat.completions.create(params)
-  console.log(chatCompletion.choices[0].message.content)
+  // console.log(chatCompletion.choices[0].message.content)
   console.log(chatCompletion.usage)
-  return {
-    response: chatCompletion.choices[0].message.content,
+
+  const ai_questions = chatCompletion.choices[0].message.content
+
+  // JSON senity
+  if (ai_questions) {
+    const jsonMatch = ai_questions?.match(/json\s*({.*})/s)
+    const fixedJsonStr = jsonMatch ? jsonMatch[1] : ai_questions
+
+    const ai_questions_json = JSON.parse(fixedJsonStr)
+    console.log(ai_questions_json)
+
+    return {
+      response: ai_questions_json,
+    }
   }
+
+  return null
 })
-
-function returnUnauthorized() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({
-        message: getRandomReturnMessage(),
-      })
-    }, 1000) // Delay for authenticity and to prevent spam
-  })
-}
-
-function getRandomReturnMessage() {
-  const messages = [
-    'Hey, looks like you\'ve stumbled upon a premium feature! Upgrade now to unlock it and get the most out of our platform.',
-    'This feature is exclusive to our premium members. Upgrade your account now to get access and enjoy all the benefits of our premium membership.',
-    'Sorry, this feature is only available to our premium members. Upgrade now to enjoy exclusive perks and access to our best features.',
-    'Hey there! This feature is one of the many benefits of our premium membership. Upgrade now to unlock it and start enjoying our premium features.',
-    'This feature is part of our premium membership package. Upgrade now to unlock it and get access to our premium features.',
-    'Sorry, you need to be a premium member to access this feature. Upgrade now to enjoy all the perks of our premium membership.',
-    'Hey, it looks like you\'re missing out on a premium feature! Upgrade now to get access and start enjoying all our premium features.',
-    'This feature is reserved for our premium members. Upgrade now to unlock it and get access to all our premium features and perks.',
-    'Sorry, this feature is only available to our premium members. Upgrade now to get access and start enjoying all the benefits of our premium membership.',
-    'Hey there! This feature is one of the many benefits of our premium membership. Upgrade now to unlock it and start enjoying our premium features.',
-    'This feature is part of our premium membership package. Upgrade now to unlock it and get access to our premium features.',
-    'Sorry, you need to be a premium member to access this feature. Upgrade now to enjoy all the perks of our premium membership.',
-    'Hey, it looks like you\'re missing out on a premium feature! Upgrade now to get access and start enjoying all our premium features.',
-  ]
-  return messages[Math.floor(Math.random() * messages.length)]
-}
-
-function checkPerms(user: User | null) {
-  if (!user)
-    return false
-  // Do some other check here, checking a custom user field, etc.
-  return true
-}
