33ef66ef78b7a6c7671694dedef112861fdae502
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-13 06:15:20 +0530

    Update UserNav.vue

--- components/core/header/components/UserNav.vue
+++ components/core/header/components/UserNav.vue
@@ -8,7 +8,6 @@ import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
