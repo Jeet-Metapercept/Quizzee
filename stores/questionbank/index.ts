4c0d655f76a14ef2b3d9b4a63caaeca216a826c4
Jeet-Metapercept <jeetp@proximabiz.com>
2023-12-14 12:25:26 +0530

    Update index.ts

--- stores/questionbank/index.ts
+++ stores/questionbank/index.ts
@@ -107,6 +107,41 @@ export const useQuestionStore = defineStore('questionStore', {
 
       return true
     },
+    async CREATE_BULK_QUESTIONS({ questions }: { questions: Omit<QuestionRow, 'id'>[] }) {
+      const client = useSupabaseClient<Database>()
+
+      // Extract question texts from the input questions
+      const questionTexts = questions.map(q => q.question.text)
+      const { data: existingQuestions } = await client
+        .from('question_bank')
+        .select('question->>text')
+        .in('question->>text', questionTexts)
+
+      // console.log('existingQuestions?', existingQuestions)
+      if (existingQuestions && existingQuestions.length > 0) {
+        const existingQuestionTexts = existingQuestions.map(q => q.text)
+        toast({ title: 'Already exists', description: `${existingQuestionTexts.join(' ,')}`, variant: 'destructive', duration: 4000 })
+        return false
+        // questions = questions.filter(q => !existingQuestionTexts.has(q.question.text))
+      }
+
+      // create
+      const { data, error } = await client
+        .from('question_bank')
+        .insert(questions)
+        .select()
+
+      if (error) {
+        toast({
+          description: error.message,
+          variant: 'destructive',
+          duration: 4000,
+        })
+        return false
+      }
+
+      return data
+    },
     async FETCH_RANDOM_QUESTIONS({ limit, category, difficulty }: {
       limit?: number
       category?: string
